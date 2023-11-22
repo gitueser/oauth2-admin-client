552415f6d7125561d8e92b8e79e8d607f9dfc26d
gitueser <jewellena78@gmail.com>
2023-11-22 13:12:40 +0100

    Initial commit

---
+++ src/main/java/com/tacocloud/oauth2adminclient/Oauth2AdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.tacocloud.oauth2adminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2AdminClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tacocloud/oauth2adminclient/Oauth2AdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacocloud.oauth2adminclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2AdminClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
